3976a3f45a9c45d03b753a483439ed2014c6a2fc
Nic Ferrier <nic@ferrier.me.uk>
2011-10-28 00:24:10 +0100

    added the GPL

--- creole.el
+++ creole.el
@@ -8,6 +8,21 @@
 ;; Version: 0.1
 ;; Keywords: lisp, creole, wiki
 
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; This is a WikiCreole wiki parser. WikiCreole is something like the
