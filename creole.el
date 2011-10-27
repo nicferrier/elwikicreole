7e10472e572d6ab085e1b157338ae6346a3685fe
Nic Ferrier <nic@ferrier.me.uk>
2011-10-27 01:14:02 +0100

    changed creole-parse to creole-tokenize (it doesn't parse) and added creole-structure to parse lists.

--- creole.el
+++ creole.el
@@ -100,7 +100,7 @@ broken over lines</a>"
 broken over lines]]"))))
 
 
-(defun creole-parse (docbuf)
+(defun creole-tokenize (docbuf)
   "Parse DOCBUF which is full of creole wiki text.
 
 See http://www.wikicreole.org/wiki/Creole1.0 for more information
@@ -172,16 +172,24 @@ Returns a list of parsed elements."
          (;; Paragraph line
           (and (looking-at "^[^=*]")
                (not (looking-at "^$")))
-          (let ((start (point))
-                (end 
-                 (save-match-data
-                   (let ((matched-end (re-search-forward "^\\(\\($\\)\\|\\([=*]\\)\\)" nil 't)))
-                     (if matched-end
-                         (- matched-end 2)
-                       (point-max))))))
-            (setq res (append res
-                              (list
-                               (cons 'para (buffer-substring start end)))))
+          (let* ((start (point))
+                 (end 
+                  (save-match-data
+                    (let* ((matched-end 
+                            ;; Find the end - the end is actually BEFORE this
+                            (re-search-forward "\\(^$\\)\\|\\(^[=*]\\)" nil 't))
+                           (matched (if matched-end (match-string 0))))
+                      (cond
+                       ((equal matched "") (- matched-end 1))
+                       ((equal matched "*") (- matched-end 2))
+                       ((equal matched "=") (- matched-end 2))
+                       (t
+                        (point-max)))))))
+            (setq res 
+                  (append 
+                   res
+                   (list
+                    (cons 'para (buffer-substring start end)))))
             (goto-char end)))
          ('t
           (forward-line))))
@@ -203,13 +211,23 @@ Returns a list of parsed elements."
     (insert "This is a paragraph {{{with code}}} and [[links]]
 and **bold** and //italics//.")))
 
+(ert-deftest creole-tokenize-newline-doc-end ()
+  "Specific test for dealing with carriage return as the end."
+  (with-temp-buffer
+    (insert "= Heading! =\n")
+    (insert "This is a paragraph {{{with code}}} and [[links]]\n")
+    (should 
+     (equal 
+      (creole-tokenize (current-buffer))
+      '((heading1 . "Heading!")
+        (para . "This is a paragraph {{{with code}}} and [[links]]"))))))
 
-(ert-deftest creole-parse ()
+(ert-deftest creole-tokenize ()
   (with-temp-buffer
     (creole--test-doc (current-buffer))
     (should 
      (equal 
-      (creole-parse (current-buffer))
+      (creole-tokenize (current-buffer))
       '((heading1 . "Heading!")
         (heading2 . "Heading2!")
         (ol1 . "an ordered list item")
@@ -227,48 +245,232 @@ and **bold** and //italics//.")))
         (para . "This is a paragraph {{{with code}}} and [[links]]
 and **bold** and //italics//."))))))
 
+(defun creole--list-item (list-symbol)
+  "Return the type and the level of the LIST-SYMBOL.
+
+For example:
+
+ (creole--list-item 'ol1) 
+  => (ordered . 1)
+
+ (creole--list-item 'ul10) 
+  => (unordered . 10)"
+  (save-match-data
+    (let ((s (symbol-name list-symbol)))
+      (when (string-match "\\([uo]l\\)\\([0-9]+\\)" s)
+        (cons
+         (intern (match-string 1 s))
+         (string-to-int (match-string 2 s)))))))
+
+(ert-deftest creole--list-item ()
+  "Test the little creole list item function."
+  (should (equal '(ul . 1) (creole--list-item 'ul1)))
+  (should (equal '(ul . 10) (creole--list-item 'ul10)))
+  (should (equal '(ul . 7) (creole--list-item 'ul7)))
+  (should (equal '(ol . 7) (creole--list-item 'ol7)))
+  (should (equal nil (creole--list-item 'h1))))
+
+(defun creole-structure (lst)
+  "Make a parsed structure from a list."
+  (let* ((docptr lst)
+         (state '()) ; used as a stack
+         (result '()))
+    (while docptr
+      (let* ((token (car docptr))
+             (lst-item (creole--list-item (car token))))
+        (case (if lst-item 'listitem (car token))
+          (listitem
+           (let* ((last (if (car state) (cdar state)))
+                  (last-level (if (car state) (caar state)))
+                  (new (list (car lst-item) (cdr token))))
+             (cond
+              ;; Current level is higher than the last, embed a new list
+              ((and last               
+                    (> (cdr lst-item) last-level))
+               (setcdr last (append (cdr last) (list new)))
+               ;; Update the stack
+               (push (cons (cdr lst-item) new) state))
+              ;; Current level is same as the last, extend the last list
+              ((and last
+                    (= (cdr lst-item) last-level))
+               (setq new (list (cdr token)))
+               (setcdr last (append (cdr last) new))
+               ;; Reset the top of the stack
+               (pop state)
+               (push (cons (cdr lst-item) new) state))
+              ;; Current level is same as the last, extend the last list
+              ((and last               
+                    (< (cdr lst-item) last-level))
+               (loop for i from 1 to (- last-level (cdr lst-item))
+                     do (pop state))
+               (let* ((last (if (car state) (cdar state)))
+                      (last-level (if (car state) (caar state))))
+                 (setq new (list (cdr token)))
+                 (setcdr last (append (cdr last) new))))
+              ;; The default action when we're dealing with lists
+              (t
+               (setq result (append result (list new)))
+               ;; Update the stack
+               (push (cons (cdr lst-item) new) state)))))
+          ;; Not a list item - just push it onto the result, always empty the list state
+          (t
+           (setq state '())
+           (setq result (append result (list token))))))
+      (setq docptr (cdr docptr)))
+    result))
+
+(ert-deftest creole-structure ()
+  (should 
+   (equal
+    (creole-structure 
+     '((h1 . "this is a heading!")
+       (ul1 . "this is a first item")
+       (ul1 . "this is a 2nd first level item")
+       (ul2 . "this is a first 2nd level item")
+       (ul2 . "this is a 2nd 2nd level item")
+       (ul3 . "this is a first 3rd level item")
+       (ul1 . "this is a return to the first level item")
+       (ul1 . "this is a 2nd first level item")
+       (ul2 . "this is a first 2nd level item")
+       (ul2 . "this is a 2nd 2nd level item")
+       (ul1 . "this is another return to first level item")))
+    '((h1 . "this is a heading!")
+      (ul
+       "this is a first item"
+       "this is a 2nd first level item"
+       (ul
+        "this is a first 2nd level item"
+        "this is a 2nd 2nd level item"
+        (ul
+         "this is a first 3rd level item"))
+       "this is a return to the first level item"
+       "this is a 2nd first level item"
+       (ul
+        "this is a first 2nd level item"
+        "this is a 2nd 2nd level item")
+       "this is another return to first level item")))))
+
+(ert-deftest creole-structure-end-to-end ()
+  (with-temp-buffer
+    (insert "= this is a heading! =\n")
+    (insert "* this is a first item\n")
+    (insert "* this is a 2nd first level item\n")
+    (insert "** this is a first 2nd level item\n")
+    (insert "** this is a 2nd 2nd level item\n")
+    (insert "*** this is a first 3rd level item\n")
+    (insert "* this is a return to the first level item\n")
+    (insert "* this is a 2nd first level item\n")
+    (insert "** this is a first 2nd level item\n")
+    (insert "** this is a 2nd 2nd level item\n")
+    (insert "* this is another return to first level item\n")
+    (insert "This is a paragraph\nthat runs over several lines\n* and a list item stops it\n")
+    (insert "This is a paragraph {{{with code}}} and [[links]]\n")
+    (should 
+     (equal
+      (creole-structure (creole-tokenize (current-buffer)))
+      '((heading1 . "this is a heading!")
+        (ul
+         "this is a first item"
+         "this is a 2nd first level item"
+         (ul
+          "this is a first 2nd level item"
+          "this is a 2nd 2nd level item"
+          (ul
+           "this is a first 3rd level item"))
+         "this is a return to the first level item"
+         "this is a 2nd first level item"
+         (ul
+          "this is a first 2nd level item"
+          "this is a 2nd 2nd level item")
+         "this is another return to first level item")
+        (para . "This is a paragraph
+that runs over several lines")
+        (ul "and a list item stops it")
+        (para . "This is a paragraph {{{with code}}} and [[links]]"))))))
+
+(defun creole--export-html-list (type lst)
+  "Export the specified LST in HTML.
+
+The exported HTML is written into the current buffer. 
+
+This is NOT intended to be used by anything but 'creole-export-html'."
+  (let ((first t))
+    (insert "<" (symbol-name type) ">\n")
+    (loop for item in lst
+          do (progn
+               (cond
+                ((listp item)
+                 (creole--export-html-list (car item) (cdr item))
+                 (setq first nil))
+                (t
+                 (if (not first)
+                     (insert "</li>\n"))
+                 (setq first nil)
+                 (insert "<li>")
+                 (insert item)))))
+    (insert "</li>\n")
+    (insert "</" (symbol-name type) ">\n")))
+
+(ert-deftest creole--export-html-list ()
+  "Test the list export, which is a little complex."
+  (with-temp-buffer
+    (creole--export-html-list 
+     'ul 
+     '("this is a list" 
+       (ul "with a deeper list") 
+       "and another item on the end"))
+    (should (equal (buffer-substring-no-properties (point-min)(point-max))
+                   "<ul>
+<li>this is a list<ul>
+<li>with a deeper list</li>
+</ul>
+</li>
+<li>and another item on the end</li>
+</ul>
+"))))
 
 (defun creole-export-html (docbuf)
   "Export DOCBUF as HTML."
-  (let ((creole (creole-parse docbuf)))
+  (let ((creole (creole-structure (creole-tokenize docbuf))))
     (with-current-buffer (get-buffer-create "*creolehtml*")
       (erase-buffer)
       (loop for element in creole
             do 
-            (case (car element)
-              ('heading1
-               (insert (format "<h1>%s</h1>\n" (cdr element))))
-              ('heading2
-               (insert (format "<h2>%s</h2>\n" (cdr element))))
-              ('heading3
-               (insert (format "<h3>%s</h3>\n" (cdr element))))
-              ('ul1
-               (insert (format "<ul>\n<li>%s</li>\n</ul>\n" 
-                               (creole-block-parse (cdr element)))))
-              ('ul2
-               (insert (format "<ul>\n<li>%s</li>\n</ul>\n" 
-                               (creole-block-parse (cdr element)))))
-              ('ul3
-               (insert (format "<ul>\n<li>%s</li>\n</ul>\n"
-                               (creole-block-parse (cdr element)))))
-              ('ol1
-               (insert (format "<ol>\n<li>%s</li>\n</ol>\n" 
-                               (creole-block-parse (cdr element)))))
-              ('ol2
-               (insert (format "<ol>\n<li>%s</li>\n</ol>\n"
-                               (creole-block-parse (cdr element)))))
-              ('ol3
-               (insert (format "<ol>\n<li>%s</li>\n</ol>\n"
-                               (creole-block-parse (cdr element)))))
-              ('preformatted
-               (insert (format "<pre>\n%s\n</pre>\n" (cdr element))))
-              ('hr
-               (insert "<hr/>\n"))
-              ('para
-               (insert (format "<p>%s</p>\n"
-                               (creole-block-parse (cdr element)))))))
+            (let ((syntax (car element)))
+              (case syntax
+                ;; The list elements can follow on from each other and require special handling
+                ((ul ol)
+                 (creole--export-html-list syntax (cdr element))
+                 )
+                (heading1
+                 (insert (format "<h1>%s</h1>\n" (cdr element))))
+                (heading2
+                 (insert (format "<h2>%s</h2>\n" (cdr element))))
+                (heading3
+                 (insert (format "<h3>%s</h3>\n" (cdr element))))
+                (preformatted
+                 (insert (format "<pre>\n%s\n</pre>\n" (cdr element))))
+                (hr
+                 (insert "<hr/>\n"))
+                (para
+                 (insert (format "<p>%s</p>\n"
+                                 (creole-block-parse (cdr element))))))))
       (current-buffer))))
 
+(ert-deftest creole-list-export ()
+  (with-temp-buffer
+    (insert "* list item
+** 2nd list item")
+    (let ((html (creole-export-html (current-buffer))))
+      (with-current-buffer html
+        (goto-char (point-min))
+        (should (looking-at "<ul>
+<li>list item<ul>
+<li>2nd list item</li>
+</ul>
+</li>
+</ul>
+"))))))
 
 (defun creole-to-html (docbuf)
   "Export the specified DOCBUF as HTML.
@@ -286,10 +488,10 @@ DOCBUF can be specified."
         (should (looking-at "<h1>Heading!</h1>
 <h2>Heading2!</h2>
 <ol>
-<li>an ordered list item</li>
-</ol>
-<ol>
+<li>an ordered list item<ol>
 <li>a 2nd ordered list item</li>
+</ol>
+</li>
 </ol>
 <h2>Heading3 is a multi word heading</h2>
 <pre>
@@ -299,19 +501,17 @@ It looks great
 }}
 </pre>
 <ul>
-<li>list item</li>
-</ul>
-<ul>
-<li>2nd list item</li>
-</ul>
-<ul>
+<li>list item<ul>
+<li>2nd list item<ul>
 <li>3rd list item</li>
 </ul>
-<ul>
-<li>another 2nd list item</li>
-</ul>
-<ul>
+</li>
+<li>another 2nd list item<ul>
 <li>another 3rd list item</li>
+</ul>
+</li>
+</ul>
+</li>
 </ul>
 <hr/>
 <p>This is a paragraph
