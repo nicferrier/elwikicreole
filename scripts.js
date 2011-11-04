985cb776de8bfd6dac9b4bd5834a9a99243d3d07
Nic Ferrier <nic@ferrier.me.uk>
2011-11-04 14:34:32 +0000

    major changes to creole to make it more customizable and to do fontification

---
+++ scripts.js
@@ -0,0 +1,1 @@
+/* Just here to make creole.el's tests pass */
---
+++ test.el
@@ -0,0 +1,7 @@
+;; A piece of elisp to htmlize
+
+(defun nic-test-10 (arg1 &key arg2)
+  (let ((x 200))
+    (format "%S" arg2)))
+
+;; End
