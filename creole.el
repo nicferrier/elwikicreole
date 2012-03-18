a5244b1dec93c09b445a29d3d80fa275cea7d983
Nic Ferrier <nic@ferrier.me.uk>
2012-03-18 18:12:35 +0000

    remember john mccarthy

--- creole.el
+++ creole.el
@@ -29,6 +29,9 @@
 ;; Wiki language used by OddMuse, which is the EmacsWiki wiki
 ;; language.
 
+;; This code was originally written to mark the death of John McCarthy
+;; - http://news.ycombinator.com/item?id=3151988
+
 ;; WikiCreole is an emerging standard documented at:
 ;; http://www.wikicreole.org/wiki/Creole1.0
 
