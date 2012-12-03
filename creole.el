0551dc01b739574e15851a94445a70df87e1a3a8
Nic Ferier <nic@ferrier.me.uk>
2012-12-03 22:31:25 +0000

    bump the version

--- creole.el
+++ creole.el
@@ -5,7 +5,7 @@
 ;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Maintainer: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Created: 27th October 2011
-;; Version: 0.8.17
+;; Version: 0.8.18
 ;; Keywords: lisp, creole, wiki
 
 ;; This file is NOT part of GNU Emacs.
