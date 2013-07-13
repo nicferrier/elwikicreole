fea37b1a2cc5d61339fab549407a486cc7ef88fb
Nic Ferrier <nferrier@ferrier.me.uk>
2013-07-13 18:59:45 +0100

    1.0.0 - FANFARE

--- creole.el
+++ creole.el
@@ -5,7 +5,7 @@
 ;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Maintainer: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Created: 27th October 2011
-;; Version: 0.9.20130602
+;; Version: 1.0.0
 ;; Package-requires: ((noflet "0.0.3")(kv "0.0.17"))
 ;; Keywords: lisp, creole, wiki
 
